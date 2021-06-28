614addd72b3150209d80d97995b457303abea6dd
JayH <b98310047@hotmail.com>
2021-06-28 11:49:46 +0800

    [ADD]
    add Task component
    add storybook/addon-storyshot to test

---
+++ src/components/Task/Task.stories.js
@@ -0,0 +1,34 @@
+import Task from '.';
+
+export default {
+	component: Task,
+	title: 'Task',
+};
+
+const Template = args => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+	task: {
+		id: '1',
+		title: 'Test Task',
+		state: 'TASK_INPUT',
+		updatedAt: new Date(2020, 0, 1, 9, 0)
+	}
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+	task : {
+		...Default.args.task,
+		state: 'TASK_PINNED',
+	}
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+	task: {
+		...Default.args.task,
+		state: 'TASK_ARCHIVED',
+	}
+};
